8e9bedf002a33e438a3a12948d3cf6f9d81b8b87
Doll Milan <ryabink.2000@icloud.com>
2021-01-19 09:18:32 +0300

    working project

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,4 +1,4 @@
-service mysql start
+service mysql start  # ?
 
 
 # /var/www/html - used for default site if client request doesn't match other sites
@@ -13,10 +13,14 @@ mv /wp-config.php /var/www/wordpress
 mv /config.inc.php /var/www/wordpress/phpmyadmin/
 # chown -R www-data:www-data /var/www/wordpress/phpmyadmin # ?
 
+# Create SSL self-signed sertificate
+# -subj '/CN=localhost'   — suppress questions about contents of sertificate
+# -nodes                  - no need for passphrase
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost' -nodes
+
 # Setup MYSQL
 mysql -u root --skip-password -e "CREATE DATABASE wordpress;"
 # GRANT ALL PRIVILGES could be set to specific SELECT, UPDATE...
-# mysql -u root --skip-password -e "CREATE USER 'super'@'localhost' IDENTIFIED BY 'password';"
 mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';"
 mysql -u root --skip-password -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root';"
 mysql -u root --skip-password -e "FLUSH PRIVILEGES;"
